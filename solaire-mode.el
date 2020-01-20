3d6f4a62609074db061e41f56d43531f41f74bc2
Henrik Lissner <henrik@lissner.net>
2020-01-19 23:53:29 -0500

    Fix header-line face remapping
    Referring to the wrong face.
    
    Reported by @ema2159

--- solaire-mode.el
+++ solaire-mode.el
@@ -150,7 +150,7 @@ line number faces will be remapped to `solaire-line-number-face'."
     ((org-indent solaire-org-hide-face)                   . t)
     ((linum solaire-line-number-face)                     . solaire-mode-remap-line-numbers)
     ((line-number solaire-line-number-face)               . solaire-mode-remap-line-numbers)
-    ((header-line solaire-mode-line-face)                 . solaire-mode-remap-headerline)
+    ((header-line solaire-header-line-face)                 . solaire-mode-remap-headerline)
     ((mode-line solaire-mode-line-face)                   . solaire-mode-remap-modeline)
     ((mode-line-inactive solaire-mode-line-inactive-face) . solaire-mode-remap-modeline)
     ((highlight-indentation-face solaire-hl-line-face)    . (featurep 'highlight-indentation)))
