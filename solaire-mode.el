2d0c98cf52b0201e34c918e86c41ecf84019026a
Henrik Lissner <git@henrik.io>
2024-04-07 12:13:32 -0400

    dev: update license year

--- solaire-mode.el
+++ solaire-mode.el
@@ -1,6 +1,6 @@
 ;;; solaire-mode.el --- make certain buffers grossly incandescent -*- lexical-binding: t; -*-
 ;;
-;; Copyright (C) 2017-2021 Henrik Lissner
+;; Copyright (C) 2017-2024 Henrik Lissner
 ;;
 ;; Author: Henrik Lissner <http://github/hlissner>
 ;; Maintainer: Henrik Lissner <contact@henrik.io>
